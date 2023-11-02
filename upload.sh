31209af81773a245d37b64f1c6f24fc7135401c8
xug15 <m13001271022@163.com>
2023-11-02 10:12:40 +0800

    Thu Nov  2 10:12:39 CST 2023

--- upload.sh
+++ upload.sh
@@ -1,7 +1,6 @@
 version=`date`
 echo ${version}
 git add *
-git commit -m "first commit"
+git commit -m "${version}"
 git branch -M main
-git remote add origin git@github.com:xug15/BCP.git
 #git push -u origin main
