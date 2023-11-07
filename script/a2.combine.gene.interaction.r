a0b1beab276dc29cfc87b57b511d104861dd2ef7
xug15 <m13001271022@163.com>
2023-11-07 13:27:56 +0800

    Tue Nov  7 13:27:54 CST 2023

--- script/a2.combine.gene.interaction.r
+++ script/a2.combine.gene.interaction.r
@@ -8,19 +8,28 @@ library(stringr)
 #read bacteria abundance file
 #pathway=read.table('d:/a-document/sequencing_center_desktop/yanglab/dynamic_project/science.abm7841_data_s1/pathannotation/CAZymes_metagenome_with_description.tsv',row.names = 1,sep="\t",header = T)
 #interaction=read.table('d:/a-document/sequencing_center_desktop/yanglab/dynamic_project/science.abm7841_data_s1/high_nutrition.interaction.tsv',sep="\t",header = T)
-interaction=read.table(args[2],sep=",",header = F)
-pathway=read.table(args[1],row.names = 1,sep="\t",header = T)
 
+#pathway=read.table('D:/a-document/github/BCP/data/annotation/CAZymes_metagenome.tsv',row.names = 1,sep="\t",header = T)
+#interaction=read.table('D:/a-document/github/BCP/data/interaction/b4.hn.interaction.csv',sep=",",header = F,skip=1)
+
+pathway=read.table(args[1],row.names = 1,sep="\t",header = T)
+interaction=read.table(args[2],sep=",",header = F,skip=1)
+
+colnames(interaction)=c('species1','species2','interaction')
 #
-head(interaction)
-tail(interaction)
-dim(interaction)
-head(pathway)
+#print("a")
+#head(interaction)
+#tail(interaction)
+#dim(interaction)
+#head(pathway)
+#print("aa")
 pathway=pathway[rowSums(pathway)>0,]
-dim(pathway)
-interaction$specise1=str_replace(interaction$specise1,"Species","s")
-interaction$specise2=str_replace(interaction$specise2,"Species","s")
-head(interaction)
+#print("ab")
+#dim(pathway)
+#head(interaction)
+interaction$species1=str_replace(interaction$species1,"Species","s")
+interaction$species2=str_replace(interaction$species2,"Species","s")
+#head(interaction)
 #find optimal lambda value that minimizes test MSE
 #pathway
 
@@ -29,11 +38,11 @@ head(interaction)
 totalnumber=dim(interaction)[1] * ((dim(pathway)[1]*2)+1)
 #
 #print(totalnumber)
-
+#print("b")
 xdata=matrix(rep(1,totalnumber),nrow=dim(interaction)[1])
-dim(xdata)
+#dim(xdata)
 for (i in seq(dim(interaction)[1]) ){
-  print(i)
+  #print(i)
   rowvalue=pathway[,interaction[i,1]]
   rowvalue=append(rowvalue,pathway[,interaction[i,2]])
   rowvalue=append(rowvalue,interaction[i,3])
@@ -42,14 +51,14 @@ for (i in seq(dim(interaction)[1]) ){
   
 }
 
-
-xdata
+#print("c")
+#xdata
 #xdata2=xdata
 colnames(xdata)=c(paste('befor',rownames(pathway)),paste('after',rownames(pathway)),"interaction")
 #colnames(xdata2)=paste('F',seq(dim(xdata)[2]),sep='')
 ydata=interaction[,3]
-head(xdata)
-dim(xdata)
+#head(xdata)
+#dim(xdata)
 
 xdata=xdata[,colSums(xdata !=0) > 0]
 #xdata2=xdata2[,colSums(xdata2 !=0) > 0]
@@ -58,24 +67,25 @@ write.csv(xdata,paste0(args[3],"ml.full.csv"),row.names = F)
 #write.csv(xdata2,'d:/a-document/sequencing_center_desktop/yanglab/dynamic_project/science.abm7841_data_s1/pathannotation/caz.train.csv',row.names = F)
 write.csv(ydata,paste0(args[3],'ml.label.csv'))
 ##
-
+#print("d")
 
 totalnumber=dim(interaction)[1]*((dim(pathway)[1])+1)
-print(totalnumber)
+#print(totalnumber)
 
 xdata_sub=matrix(rep(1,totalnumber),nrow=dim(interaction)[1])
 for (i in seq(dim(interaction)[1]) ){
-  print(i)
+  #print(i)
   rowvalue=pathway[,interaction[i,1]]-pathway[,interaction[i,2]]
   rowvalue=append(rowvalue,interaction[i,3])
   
   xdata_sub[i,]=rowvalue
   
 }
+#print("f")
 colnames(xdata_sub)=c(rownames(pathway),"interaction")
 xdata_sub=xdata_sub[,colSums(xdata_sub !=0) > 0]
 #xdata_sub2=xdata_sub
 #colnames(xdata_sub2)=append(paste('F',seq(dim(xdata_sub2)[2]-1),sep=''),'interaction')
-write.csv(xdata_sub,paste0(argv[3],".ml.sub.csv"),row.names = F)
+write.csv(xdata_sub,paste0(args[3],".ml.sub.csv"),row.names = F)
 #write.csv(xdata_sub2,'d:/a-document/sequencing_center_desktop/yanglab/dynamic_project/science.abm7841_data_s1/pathannotation/caz.sub.train.csv',row.names = F)
 ##
