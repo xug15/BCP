c5043eeba56c8b7c03e486cf3660d8b776b310b8
xug15 <m13001271022@163.com>
2024-01-17 14:27:21 +0800

    Wed Jan 17 14:27:06 CST 2024

--- upload.sh
+++ upload.sh
@@ -1,3 +1,10 @@
+echo git config --global user.name "your_github_username"
+echo git config --global user.email "your_github_email"
+echo git config -l
+echo git remote -v
+echo git remote set-url origin git@github.com:USERNAME/REPONAME.git
+
+
 version=`date`
 echo ${version}
 git add -A
